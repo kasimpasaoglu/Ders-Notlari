9ab2448d31890f30ee86ac25a4f1e52b45c79ee9
Kasım Paşaoğlu <kasimpasaoglu@windowslive.com>
2024-09-12 01:16:41 +0300

    fh

---
+++ CALISMALAR/Math-Odev/Program.cs
@@ -0,0 +1,302 @@
+﻿
+
+// {
+//     Console.WriteLine("Pozitife Cevirmek Istediginiz Tam Sayiyi Girin:");
+//     Console.WriteLine("Sayinin Pozitife donusumu = " + Math.Abs(Convert.ToInt32(Console.ReadLine())));
+// }
+
+
+
+
+// {
+//     Console.WriteLine("Acisi Hesaplanacak(radyan) Kosinus degerini giriniz (-1<cos<1) :");
+//     double userNumber = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+//     Console.WriteLine("Radyan = " + Math.Acos(userNumber));
+// }
+
+
+
+
+// {
+//     Console.WriteLine("Ters Hiperbolik Kosinus hesapalamak icin deger girin (min deger '1'):");
+//     double userNumber = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+//     Console.WriteLine("Ters Hiperbolik Kosinus : " + Math.Acosh(userNumber));
+// }
+
+
+
+
+// {
+//     System.Console.WriteLine("Kuvveti alinacak sayiyi giriniz :");
+//     double baseNumber = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+//     System.Console.WriteLine("Kuvveti giriniz :");
+//     double powerNumber = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+//     double result = Math.Pow(baseNumber, powerNumber);
+//     Console.WriteLine("{0} 'in {1} 'inci kuvveti = {2}", baseNumber, powerNumber, result);
+// }
+
+
+
+
+// {
+//     Console.WriteLine("-- En buyuk ve en kucuk sayiyi bulma --");
+//     System.Console.WriteLine("1. Sayiyi Girin :");
+//     double number1 = Convert.ToDouble(Console.ReadLine());
+//     System.Console.WriteLine("2. Sayiyi Girin :");
+//     double number2 = Convert.ToDouble(Console.ReadLine());
+//     System.Console.WriteLine("3. Sayiyi Girin :");
+//     double number3 = Convert.ToDouble(Console.ReadLine());
+//     System.Console.WriteLine("4. Sayiyi Girin :");
+//     double number4 = Convert.ToDouble(Console.ReadLine());
+//     System.Console.WriteLine("5. Sayiyi Girin :");
+//     double number5 = Convert.ToDouble(Console.ReadLine());
+//     System.Console.WriteLine("6. Sayiyi Girin :");
+//     double number6 = Convert.ToDouble(Console.ReadLine());
+//     System.Console.WriteLine("7. Sayiyi Girin :");
+//     double number7 = Convert.ToDouble(Console.ReadLine());
+
+//     double biggestNumber =
+//     Math.Max(number1,
+//     Math.Max(number2,
+//     Math.Max(number3,
+//     Math.Max(number4,
+//     Math.Max(number5,
+//     Math.Max(number6, number7))))));
+
+//     double smallestNumber =
+//     Math.Min(number1,
+//     Math.Min(number2,
+//     Math.Min(number3,
+//     Math.Min(number4,
+//     Math.Min(number5,
+//     Math.Min(number6, number7))))));
+
+//     Console.WriteLine("En kucuk sayi = " + smallestNumber);
+//     Console.WriteLine("En buyuk sayi = " + biggestNumber);
+// }
+
+// {
+//     Console.WriteLine(Math.DivRem(10, 3)); // out int ramainder ???
+// }
+
+
+
+// DETAYLI UCGEN COZUMLEME
+// {
+//     Console.WriteLine("Bir ucgenin alanini ve 3. kenar uzunlugunu bulma...");
+
+//     Console.WriteLine("Birinci Kenar Uzunlugunu Giriniz (A) : ");
+//     double sideA = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Ikinci Kenar Uzunlugunu Giriniz (B): ");
+//     double sideB = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Bu iki kenar arasindaki Aciyi Giriniz (c): ");
+//     double degreesC = Convert.ToDouble(Console.ReadLine());
+
+//     // HESAPLAMALAR
+
+//     // derece olarak gelen aciyi radyan cinsine cevir
+//     double radiansC = degreesC * (Math.PI / 180);
+
+//     // kosinus teoremi ile ucuncu kenari bulma (Kok icinde; A^2 + B^2 - 2(A*B*Cos(c)))
+//     double sideC = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2) - 2 * sideA * sideB * Math.Cos(radiansC));
+
+//     // alan hesaplama
+//     double area = (sideA * sideB * Math.Sin(radiansC) / 2);
+
+//     // Cevresini Hesapla
+//     double perimeter = sideA + sideB + sideC;
+
+//     // sinus kurali ile diger acilari hesaplama (arcsin(B/C * sin(c) * `Radyani Dereceye cevir`)
+
+//     double degreesB = (Math.Asin((sideB / sideC) * Math.Sin(radiansC))) * (180 / Math.PI);
+
+//     double degreesA = 180 - degreesB - degreesC;
+
+//     // siniflandirma
+
+//     double longSide = Math.Max(sideA, Math.Max(sideB, sideC));
+//     double shortSide = Math.Min(sideA, Math.Min(sideB, sideC));
+//     double middleSide = perimeter - longSide - shortSide;
+
+//     double largeAngle = Math.Max(degreesA, Math.Max(degreesB, degreesC));
+//     double smallAngle = Math.Min(degreesA, Math.Min(degreesB, degreesC));
+//     double middleAngle = 180 - largeAngle - smallAngle;
+
+//     // sonuclar
+//     Console.WriteLine(" Uzun Kenar {0} => Karisindaki Aci {1} ", longSide, largeAngle);
+//     Console.WriteLine(" Kisa Kenar {0} => Karisindaki Aci {1} ", shortSide, smallAngle);
+//     Console.WriteLine(" Orta Kenar {0} => Karisindaki Aci {1} ", middleSide, middleAngle);
+//     Console.WriteLine(" Cevresi {0} ve Alani {1} ", perimeter, area);
+// }
+
+
+
+// {
+//     System.Console.WriteLine("Bir Koninin Hacmi ve yuzey alani hesaplama...");
+
+//     Console.WriteLine("Taban Yaricapini Giriniz (r) :");
+//     double radius = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Yuksekligi Giriniz (h)");
+//     double height = Convert.ToDouble(Console.ReadLine());
+
+//     // pisagor ile yan yuz uzunlugu bul
+
+//     double hypotenuse = Math.Sqrt(Math.Pow(radius, 2) + Math.Pow(height, 2));
+
+//     double volume = ((Math.PI * Math.Pow(radius, 2) * height) / 3);
+
+//     double surfaceArea = Math.PI * radius * (radius + hypotenuse);
+//     Console.WriteLine("Yaricapi {0} ve yuksekligi {1} olan koninin, hacmi {2} ve yuzey alani {3} olur...", radius, height, volume, surfaceArea);
+// }
+
+
+
+// // BIR NESNENIN ACISAL HIZINI KOORDINATLARINI HESAPLAMA
+// {
+//     Console.WriteLine("Daire Icinde Bir Noktanin Acisal Hizini, Konumunu Hesaplama");
+
+//     Console.WriteLine("Dairenin yaricapini girin (r) :");
+//     double radius = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Nesnenin cizgisel hizini girin (v)");
+//     double vectoralVelocity = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Gecen Sureyi giriniz (t):");
+//     double time = Convert.ToDouble(Console.ReadLine());
+
+//     double angularVelocity = vectoralVelocity / radius;
+
+//     double turnedAngle = angularVelocity * time;
+//     double turnedAngleDegree = turnedAngle * (180 / Math.PI);
+
+//     double coordinateX = radius * Math.Cos(turnedAngle);
+//     double coordinateY = radius * Math.Sin(turnedAngle);
+
+//     Console.WriteLine("Nesne baslangicta (x= {0}, y= {1}) koordinatlarinda", radius, 0);
+//     Console.WriteLine("Nesnenin vektorel hizi = {0}, acisal hizi = {1}", vectoralVelocity, angularVelocity);
+//     Console.WriteLine("Hareket Suresi = {0}, toplam donus acisi = {1}", time, turnedAngleDegree);
+//     Console.WriteLine("Nesne hareketi tamamladiktan sonra (x= {0}, y= {1}) koordinatlarinda", coordinateX, coordinateY);
+
+// }
+
+
+
+
+// {
+//     Console.WriteLine("--Bilesik Faiz Hesaplama--");
+
+//     Console.WriteLine("Baslangic Yatirim tutarini girin (P):");
+//     int principal = Convert.ToInt32(Console.ReadLine());
+
+//     Console.WriteLine("Yillik faiz oranini girin (r):");
+//     double interestRate = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+//     Console.WriteLine("Yil Sayisi Girin (t):");
+//     double years = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Faiz Periyodu Sayisini Giriniz (n) :");
+//     int compoundingPeriodsPerYear = Convert.ToInt32(Console.ReadLine());
+
+//     double futureValue = principal * Math.Pow(1 + interestRate / compoundingPeriodsPerYear, compoundingPeriodsPerYear * years);
+
+//     Console.WriteLine("Vade Sonu Toplam Tutar = {0}", futureValue);
+
+// }
+
+
+
+// {
+//     Console.WriteLine("--Hedef Tutara Ulasmak Icin Gereken Yil Zamani--");
+
+//     Console.WriteLine("Baslangic Yatirim tutarini girin (P):");
+//     double principal = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Gelecekteki Hedef Degeri Girin (A):");
+//     double expectedFutureValue = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Yillik faiz oranini girin (r):");
+//     double interestRate = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+//     Console.WriteLine("Faiz Periyodu Sayisini Giriniz (n) :");
+//     double compoundingPeriodsPerYear = Convert.ToDouble(Console.ReadLine());
+
+//     double yearsReq = Math.Log(expectedFutureValue / principal) / (compoundingPeriodsPerYear * Math.Log(1 + (interestRate / compoundingPeriodsPerYear)));
+
+//     Console.WriteLine("{0} Tutarindaki yatirimin {1} faizle, {2} tutarina ulasmasi icin, {3} periotlarla toplam {4} yil vadeye yatirilmasi gerekmektedir", principal, interestRate, expectedFutureValue, compoundingPeriodsPerYear, yearsReq);
+// }
+
+
+
+// {
+//     Console.WriteLine();
+//     Console.WriteLine("--Iki Nokta Arasindaki Mesafeyi ve Orta Noktayi Bulma--");
+
+//     Console.WriteLine("Ilk noktanin x koordinatini girin :");
+//     double startingPointX = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Ilk noktanin y koordinatini girin :");
+//     double startingPointY = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Ikinci noktanin x koordinatini girin :");
+//     double endingPointX = Convert.ToDouble(Console.ReadLine());
+
+//     Console.WriteLine("Ikinci noktanin y koordinatini girin :");
+//     double endingPointY = Convert.ToDouble(Console.ReadLine());
+
+//     double distance = Math.Sqrt(Math.Pow(endingPointX - startingPointX, 2) + Math.Pow(endingPointY - startingPointY, 2));
+
+//     double middleX = (startingPointX + endingPointX) / 2;
+//     double middleY = (startingPointY + endingPointY) / 2;
+
+//     Console.WriteLine("(X:{0}, Y:{1}) koordinatlarindan baslayip, (X:{2}, Y:{3}) koordinatlarinda biten dogrunun uzunlugu {4} olur. Orta noktasinin koordinatlari ise (X:{5}, Y:{6}) olur.", startingPointX, startingPointY, endingPointX, endingPointY, distance, middleX, middleY);
+
+// }
+
+
+
+{
+    Console.WriteLine();
+    Console.WriteLine("--Bakteri Populasyonunu Buyumesini Hesaplama--");
+    Console.WriteLine();
+
+    Console.WriteLine("Baslangic Populasyonunu Yaziniz :");
+    int initPopulation = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Buyume Oranini Yaziniz :");
+    double growthRate = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+    Console.WriteLine("Sureyi yaziniz (saat)");
+    double time = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+    double finalPopulation = Math.Round(initPopulation * Math.Pow((1 + growthRate), time), 2);
+    Console.WriteLine($"{time} saat sonra populasyin {finalPopulation} olacaktir");
+
+    Console.WriteLine();
+
+    Console.WriteLine("Hedeflenen Populasyonu Girin");
+    double targetPopulation = Convert.ToDouble(Console.ReadLine());
+
+    double reqTime = Math.Round(Math.Log(targetPopulation / initPopulation) / Math.Log(1 + growthRate), 2);
+
+    int hours = Convert.ToInt32(Math.Floor(reqTime));  // sadece saati al
+
+    double decimalPartOfTime = reqTime - hours;   // reqTime icindeki ondalik kismi al
+
+    double minutesWithDecimal = decimalPartOfTime * 60;   // ondalik kismi 60 a carparak dakikaya cevir
+
+    int minutes = Convert.ToInt32(Math.Floor(minutesWithDecimal));  // dakika icin ondalik kismi at, minutes olarak kaydet
+
+    double decimalPartOfMinutes = minutesWithDecimal - minutes;   //  ondalikli dakika icindeki ondalik bolumunu al
+
+    int seconds = Convert.ToInt32(Math.Ceiling(decimalPartOfMinutes * 60)); // ondalikli dakika icindeki ondalik bolumunu 60 a carpip saniyeye cevir, ve ondalik kismini at.
+
+    Console.WriteLine("{0} Hedefinde ulasmak icin {1} saat, {2} dakika, {3} saniye gereklidir... ", targetPopulation, hours, minutes, seconds);
+}
+
+// double number = 1.5;
+// double number = 1,5;
+
+Math.Exp(10);
---
+++ CALISMALAR/parse-tostring-calisma/Program.cs
@@ -0,0 +1,232 @@
+﻿
+#region Ucgen Cozumleme;
+/*
+Console.WriteLine("A Kenari Uzunlugunu Girin :");
+double sideA = double.Parse(Console.ReadLine().Replace('.', ','));
+Console.WriteLine();
+
+Console.WriteLine("B Kenari Uzunlugunu Girin :");
+double sideB = double.Parse(Console.ReadLine().Replace('.', ','));
+Console.WriteLine();
+
+Console.WriteLine("c Acisi Derecesini Girin :");
+double angleC = double.Parse(Console.ReadLine().Replace('.', ','));
+Console.WriteLine();
+
+double radianC = angleC * Math.PI / 180;
+
+double sideC = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2) - (2 * sideA * sideB * Math.Cos(radianC)));
+
+double radianA = Math.Asin(sideA * Math.Sin(radianC) / sideC);
+double radianB = Math.Asin(sideB * Math.Sin(radianC) / sideC);
+
+double angleA = radianA * 180 / Math.PI;
+double angleB = radianB * 180 / Math.PI;
+
+// ondaliklari yuvarladiktan sonrasinda stringe donusturmeye gerek yok ama pratik amacli yaptim
+string roundedSideA = Math.Round(sideA, 2).ToString();
+string roundedSideB = Math.Round(sideB, 2).ToString();
+string roundedSideC = Math.Round(sideC, 2).ToString();
+
+string roundedAngleA = Math.Round(angleA, 2).ToString();
+string roundedAngleB = Math.Round(angleB, 2).ToString();
+string roundedAngleC = Math.Round(angleC, 2).ToString();
+
+string interiorAngleSum = Math.Round(angleA + angleB + angleC, 2).ToString();
+string perimeter = Math.Round(sideA + sideB + sideC, 2).ToString();
+string area = Math.Round(sideA * sideB * Math.Sin(radianC) / 2, 2).ToString();
+
+
+Console.WriteLine($"Ucgenin Kenar Uzunluklari => A:{roundedSideA} , B:{roundedSideB}, C:{roundedSideC}");
+Console.WriteLine();
+Console.WriteLine($"Ucgenin Ic Acilari => a: {roundedAngleA} , b: {roundedAngleB}, c: {roundedAngleC}, Toplami = {interiorAngleSum}");
+Console.WriteLine();
+Console.WriteLine($"Ucgenin Cevresi => {perimeter}");
+Console.WriteLine();
+Console.WriteLine($"Ucgenin Alani => {area}");
+*/
+#endregion
+
+#region Kullanici Bilgileri ve Faiz Hesaplama
+/*
+Console.WriteLine("Ad & Soyad Girin :");
+string fullName = Console.ReadLine().Trim();
+
+Console.WriteLine("Dogum Yilinizi Girin :");
+int birthYear = int.Parse(Console.ReadLine());
+
+
+int index = fullName.LastIndexOf(' ');
+
+string name = fullName.Substring(0, index).ToUpper();
+string lastName = fullName.Substring(index + 1).ToUpper();
+
+int age = DateTime.Now.Year - birthYear;
+string account = (age > 18).ToString().Replace("True", "Yetkili").Replace("False", "Ebeveyn Denetimi Gerekli");
+
+string userName = (name.Substring(0, 1) + '.' + lastName).ToLower();
+
+Random rnd = new Random();
+string intRandom = rnd.Next(10000, 99999).ToString();
+
+string userID = intRandom.Substring(0, 1) + name.Substring(0, 1).ToLower() + intRandom.Substring(2) + lastName.Substring(0, 1).ToLower();
+
+double accBalance = Math.Round(rnd.NextDouble() * 10000, 2);
+
+Console.WriteLine();
+Console.WriteLine("Adiniz : {0}", name);
+Console.WriteLine("Soyadiniz : {0}", lastName);
+Console.WriteLine("Kullanici Adi : {0}", userName);
+Console.WriteLine("Kullanici Kodu : {0}", userID);
+Console.WriteLine("Yasiniz : {0}", age);
+Console.WriteLine("Hesap : {0}", account);
+Console.WriteLine("Hesap Bakiyesi : {0}", accBalance);
+Console.WriteLine();
+
+Console.WriteLine("Vade Baslangic Bakiyesini Girin (En Dusuk Tutar 10000 TL):");
+double principalRequested = double.Parse(Console.ReadLine());
+
+double reqAmount = principalRequested - accBalance;
+Console.WriteLine("Bu islem icin Vadeli Hesabiniza {0} TL aktarilacaktir.", reqAmount);
+accBalance = accBalance + reqAmount;
+
+Console.WriteLine();
+Console.WriteLine("Transfer Tamamlandi... Yeni Hesap Bakiyesi: {0}", accBalance);
+Console.WriteLine();
+
+Console.WriteLine("Hesaplanacak faiz oranini giriniz :");
+double interestRate = double.Parse(Console.ReadLine().Replace('.', ','));
+
+Console.WriteLine("Vade sayisi giriniz (YIL) :");
+double years = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Vade Periodu Giriniz (AY) :");
+double compoundingPeriods = double.Parse(Console.ReadLine());
+
+double finalAmount = Math.Round(accBalance * Math.Pow(1 + interestRate / compoundingPeriods, compoundingPeriods * years), 3);
+
+Console.WriteLine("Anapara => {0} TL", accBalance);
+Console.WriteLine("Faiz Orani => %{0}", interestRate * 100);
+Console.WriteLine("Vade Sayisi => {0} Yil", years);
+Console.WriteLine("Vade Periodu => {0} Ay", compoundingPeriods);
+Console.WriteLine("Vade Sonu Hesaplanan Tutar => {0}", finalAmount);
+*/
+#endregion
+
+#region Not Sistemi
+// Random rnd = new Random();
+
+// Console.WriteLine("1 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName1 = Console.ReadLine().Trim();
+// Console.WriteLine("1 . Ogrencinin Notunu Giriniz :");
+// double studentNote1 = double.Parse(Console.ReadLine());
+// int studentID1 = rnd.Next(1000, 2000);
+
+// Console.WriteLine("2 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName2 = Console.ReadLine().Trim();
+// Console.WriteLine("2 . Ogrencinin Notunu Giriniz :");
+// double studentNote2 = double.Parse(Console.ReadLine());
+// int studentID2 = rnd.Next(1000, 2000);
+
+// Console.WriteLine("3 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName3 = Console.ReadLine().Trim();
+// Console.WriteLine("3 . Ogrencinin Notunu Giriniz :");
+// double studentNote3 = double.Parse(Console.ReadLine());
+// int studentID3 = rnd.Next(1000, 2000);
+
+// Console.WriteLine("4 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName4 = Console.ReadLine().Trim();
+// Console.WriteLine("4 . Ogrencinin Notunu Giriniz :");
+// double studentNote4 = double.Parse(Console.ReadLine());
+// int studentID4 = rnd.Next(1000, 2000);
+
+// Console.WriteLine("5 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName5 = Console.ReadLine().Trim();
+// Console.WriteLine("5 . Ogrencinin Notunu Giriniz :");
+// double studentNote5 = double.Parse(Console.ReadLine());
+// int studentID5 = rnd.Next(1000, 2000);
+
+// Console.WriteLine("6 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName6 = Console.ReadLine().Trim();
+// Console.WriteLine("6 . Ogrencinin Notunu Giriniz :");
+// double studentNote6 = double.Parse(Console.ReadLine());
+// int studentID6 = rnd.Next(1000, 2000);
+
+// Console.WriteLine("7 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName7 = Console.ReadLine().Trim();
+// Console.WriteLine("7 . Ogrencinin Notunu Giriniz :");
+// double studentNote7 = double.Parse(Console.ReadLine());
+// int studentID7 = rnd.Next(1000, 2000);
+
+// Console.WriteLine("8 . Ogrencinin Adini/Soyadini Giriniz :");
+// string studentName8 = Console.ReadLine().Trim();
+// Console.WriteLine("8 . Ogrencinin Notunu Giriniz :");
+// double studentNote8 = double.Parse(Console.ReadLine());
+// int studentID8 = rnd.Next(1000, 2000);
+
+// double maxNote = Math.Max(studentNote1, Math.Max(studentNote2, Math.Max(studentNote3, Math.Max(studentNote4, Math.Max(studentNote5, Math.Max(studentNote6, Math.Max(studentNote7, studentNote8)))))));
+
+// double minNote = Math.Min(studentNote1, Math.Min(studentNote2, Math.Min(studentNote3, Math.Min(studentNote4, Math.Min(studentNote5, Math.Min(studentNote6, Math.Min(studentNote7, studentNote8)))))));
+
+// string isStudentPassed1 = (studentNote1 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+// string isStudentPassed2 = (studentNote2 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+// string isStudentPassed3 = (studentNote3 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+// string isStudentPassed4 = (studentNote4 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+// string isStudentPassed5 = (studentNote5 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+// string isStudentPassed6 = (studentNote6 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+// string isStudentPassed7 = (studentNote7 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+// string isStudentPassed8 = (studentNote8 >= 70).ToString().Replace("True", "Gecti").Replace("False", "Kaldi");
+
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName1, isStudentPassed1, studentID1, studentNote1);
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName2, isStudentPassed2, studentID2, studentNote2);
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName3, isStudentPassed3, studentID3, studentNote3);
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName4, isStudentPassed4, studentID4, studentNote4);
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName5, isStudentPassed5, studentID5, studentNote5);
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName6, isStudentPassed6, studentID6, studentNote6);
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName7, isStudentPassed7, studentID7, studentNote7);
+// Console.WriteLine("Ogrenci No:{2} = {0} = {3} => {1}", studentName8, isStudentPassed8, studentID8, studentNote8);
+// Console.WriteLine();
+// Console.WriteLine("Sinifin Not Ortalamasi ==> {0}", (studentNote1 + studentNote2 + studentNote3 + studentNote4 + studentNote5 + studentNote6 + studentNote7 + studentNote8) / 8);
+// Console.WriteLine("En Yuksek Not => {0} ; En Dusuk Not => {1}", maxNote, minNote);
+
+#endregion
+
+#region Uzunluk Birimi Donusturucu
+
+// Console.WriteLine("Olcuyu Giriniz");
+
+// double userInputMeter = double.Parse(Console.ReadLine().Trim());
+
+// double decimeter = userInputMeter * 10;
+// double centimeter = userInputMeter * 100;
+// double milimeter = userInputMeter * 1000;
+// double kilometer = Math.Round(userInputMeter / 1000, 2);
+// double mile = Math.Round(userInputMeter / 1609.34, 2);
+// double yard = Math.Round(userInputMeter / 0.9144, 2);
+// double foot = Math.Round(userInputMeter / 0.3048, 2);
+// double inch = Math.Round(userInputMeter / 0.0254, 2);
+
+// Console.WriteLine("{0} Metre => {1}dm = {2}cm = {3}mm = {4}km = {5}mile = {6}yard = {7}foot = {8}inch", userInputMeter, decimeter, centimeter, milimeter, kilometer, mile, yard, foot, inch);
+
+// Console.WriteLine();
+
+#endregion
+
+#region Cember, Daire, Silindir Cozumleme
+
+// Console.WriteLine("Yaricapi olcusunu girin :");
+// double radius = double.Parse(Console.ReadLine());
+
+// Console.WriteLine("Yuksekligini girin :");
+// double height = double.Parse(Console.ReadLine());
+
+// double perimeter = Math.Round(2 * Math.PI * radius, 2);
+
+// double area = Math.Round(Math.PI * Math.Pow(radius, 2), 2);
+
+// double volume = Math.Round(Math.PI * Math.Pow(radius, 2) * height, 2);
+
+// Console.WriteLine("Cevresi = {0} , Alani = {1}, Silindirin Hacmi = {2}", perimeter, area, volume);
+
+
+#endregion
