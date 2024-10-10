4ba33f144601d4fc062eddb5f94426786cc1760e
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2024-10-10 19:49:24 +0300

    asd

--- fonksiyonlar-deger-referans tipi-010-1/Program.cs
+++ fonksiyonlar-deger-referans tipi-010-1/Program.cs
@@ -95,6 +95,52 @@ Console.WriteLine(a); // ve a degiskenine 0 atayacak
 */
 #endregion
 
-#region 
+#region Metot OverLoading
+/*
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+    }
+    static void Deneme(int a)
+    {
 
+    }
+
+    static void Deneme(string a, string b)
+    {
+
+    }
+}
+*/
+
+#endregion
+
+#region Params
+/*
+static void SinirsizParametre(params int[] parameters)
+{
+    var toplam = 0;
+    foreach (var item in parameters)
+    {
+        toplam += item;
+    }
+    Console.WriteLine($"Toplam => {toplam} || Ortalama => {toplam / parameters.Length}");
+}
+
+SinirsizParametre(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+*/
+#endregion
+
+#region 
+static void Metod1(int a = 10)
+{
+    Console.WriteLine(a);
+}
+
+Metod1();
+static void Metod2(string value = "")
+{
+
+}
 #endregion
