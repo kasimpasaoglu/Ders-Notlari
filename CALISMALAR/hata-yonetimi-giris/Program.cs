29bf6a7b74c75ac884b7d5e3f4c0f2bfef52bc45
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2024-10-11 15:46:36 +0300

    asd

--- CALISMALAR/hata-yonetimi-giris/Program.cs
+++ CALISMALAR/hata-yonetimi-giris/Program.cs
@@ -35,22 +35,47 @@ Console.WriteLine(Math.Round(result, 2));
 
 while (true)
 {
-    Console.WriteLine("Birinci Sayiyi Giriniz");
-    double firstNumber = GetNumber();
-    Console.WriteLine("Ikinci Sayiyi Giriniz");
-    double secondNumber = GetNumber();
-
     int process = GetProcess();
 
-    if (process == 6)
+    double firstNumber;
+    double secondNumber;
+    if (process == 1 || process == 2 || process == 3)
     {
-        GetRoot(ref firstNumber, ref secondNumber);
-    }
-    else
-    {
+        Console.WriteLine("Birinci Sayiyi Giriniz");
+        firstNumber = GetNumber();
+        Console.WriteLine("Ikinci Sayiyi Giriniz");
+        secondNumber = GetNumber();
         SimpleProcess(firstNumber, ref secondNumber, process);
     }
-
+    else if (process == 4)
+    {
+        Console.WriteLine("Bolunen Sayiyi Giriniz");
+        firstNumber = GetNumber();
+        Console.WriteLine("Bolen Sayiyi Giriniz");
+        secondNumber = GetNumber();
+        SimpleProcess(firstNumber, ref secondNumber, process);
+    }
+    else if (process == 5)
+    {
+        Console.WriteLine("Sayiyi Giriniz");
+        firstNumber = GetNumber();
+        Console.WriteLine("Kuvvetini Giriniz");
+        secondNumber = GetNumber();
+        SimpleProcess(firstNumber, ref secondNumber, process);
+    }
+    else if (process == 6)
+    {
+        Console.WriteLine("Sayiyi Giriniz");
+        firstNumber = GetNumber();
+        Console.WriteLine("Kok Derecesini Giriniz");
+        secondNumber = GetNumber();
+        GetRoot(ref firstNumber, ref secondNumber);
+    }
+    else if (process == 7)
+    {
+        Console.WriteLine("Faktoriyel Alinacak Sayiyi Giriniz");
+        GetFactorial(GetNumber());
+    }
     if (isExiting())
     {
         break;
@@ -67,10 +92,11 @@ static int GetProcess()
     Console.WriteLine("4. Bolme");
     Console.WriteLine("5. Kuvvetini Alma");
     Console.WriteLine("6. Kok Alma");
+    Console.WriteLine("7. Faktoriyel Hesaplama");
 
     int process;
 
-    while (!int.TryParse(Console.ReadLine().Trim(), out process) || process > 6 || process < 1)
+    while (!int.TryParse(Console.ReadLine().Trim(), out process) || process > 7 || process < 1)
     {
         Console.WriteLine("Lutfen Gecerli Bir Secim Yapiniz");
     }
@@ -193,22 +219,53 @@ static void GetRoot(ref double a, ref double b)
     }
 }
 
+static void GetFactorial(double a)
+{
+    try
+    {
+        decimal result = 1;
+        for (int i = 1; i < a; i++)
+        {
+            result *= i;
+        }
+        Console.WriteLine($"{a}! = {Math.Round(result, 2)}");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("HATA: Sonuc, Hesaplanabilecek Sayi Araligindan Daha Buyuk");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Beklenmedik Bir Hata Olustu =>> " + ex.Message);
+    }
+}
+
 static bool isExiting()
 {
     Console.WriteLine("Baska Bir Islem Yapmak Icin Enter'a Basiniz");
     Console.WriteLine("Cikmak Icin 'exit' Yaziniz");
     var input = Console.ReadLine().Trim().ToLower();
-    while (input != "" && input != "exit")
+    while (true)
     {
-        Console.WriteLine("Girilen Komut Bulunamadi");
-        input = Console.ReadLine().Trim().ToLower();
+        if (input == "" || input == "exit")
+        {
+            break;
+        }
+        else
+        {
+
+            Console.WriteLine("Girilen Komut Bulunamadi"); // 
+            input = Console.ReadLine().Trim().ToLower();
+        }
     }
-    if (input == "exit")
+    if (input == "")
+    {
+        return false;
+    }
+    else
     {
         return true;
     }
-    return false;
 }
-
 #endregion
 
