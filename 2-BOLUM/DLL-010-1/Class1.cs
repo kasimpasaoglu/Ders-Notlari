b83d6cb25c4099a76d43f836768c40b4678f5844
Kasım Paşaoğlu <168188063+kasimpasaoglu@users.noreply.github.com>
2024-10-27 11:42:50 +0300

    ss

---
+++ 2-BOLUM/DLL-010-1/Class1.cs
@@ -0,0 +1,20 @@
+﻿// dotnet new classlib komutu ile bir class liblary olusturduk
+// exe ile dll arasindaki en onemli fark dll' de main metodu yoktur. bu yuzden dll dosyalari tek baslarina calistirilamazlar.
+// zaten varolus amaclari baska uygulamalara yardimci olmaktir, calistirilmak degil.
+
+namespace DLL_KodKutuphanesi;
+
+public class Ogrenci
+{
+
+    public void Calis()
+    {
+        Console.WriteLine("Ogrenci Calisti");
+    }
+    public void Uyu()
+    {
+        Console.WriteLine("Ogrenci Uyudu");
+    }
+}
+
+// kodlama bittikten sonra `dotnet build` komutu ile dll dosyasini olustururuz
---
+++ 2-BOLUM/access-modifiers-011/Program.cs
@@ -0,0 +1,17 @@
+﻿// Access Modifier bir uyeye erisimin nasil olacagini belirler.
+/// public : Her yerden ve her katmandan bu uyeye erisim saglanabilir
+/// /// Her uye icin verilebilir
+/// /// Class-Metod-Prop-Ctor-Degiskenler access modifier alabilirler
+
+/// private : Sadece o uyenin oldugu katmandan erisim saglanabilir
+/// /// her uye icin verilebilir
+/// /// classlar private olamazlar
+/// /// private uyeler kalitim ile baska bir classa gitmez
+/// /// erisim belirtec yazilmaz ise default erisim belirtec `private`dir
+
+/// protected : yazilan uyenin, yazildigi sinif, baska bir sinifa kalitildigi zaman, yeni siniftan gorunebilir olmasini, onun disinda gorunmemesini saglar.
+/// protected, private gibidir ancak tek farkli, kalitim yolu ile baska classlarda erisilebilir olmasini saglar. 
+
+/// internal : Kutuphaneler icerisinde birbirine ulas, farkli kutuphane ise ulasma. 
+/// /// Kutuphane nedir? (DLL kod kutuphanesi Ara Konu)
+/// 
