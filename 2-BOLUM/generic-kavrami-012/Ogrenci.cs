cbb0e9a5f751a6666e04b2928c24d3243d450a7a
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2024-10-30 21:56:32 +0300

    asd

--- 2-BOLUM/generic-kavrami-012/Ogrenci.cs
+++ 2-BOLUM/generic-kavrami-012/Ogrenci.cs
@@ -1,5 +1,36 @@
 public class Ogrenci
 {
-    public int Id { get; set; }
+    public int Id { get; }
     public string Name { get; set; }
+    public Ogrenci(string name)
+    {
+        Id = new Random().Next(1000, 2000);
+        Name = name;
+    }
+
+    public static int Menu()
+    {
+        Console.WriteLine("1-Ogrenci Ekle");
+        Console.WriteLine("2-Ogrenci Sil");
+        Console.WriteLine("3-Ogrencileri Yazdir");
+        Console.WriteLine("4-Cikis Yap");
+        int input;
+        while (!(int.TryParse(Console.ReadLine().Trim(), out input) && (input == 1 || input == 2 || input == 3 || input == 4)))
+        {
+            Console.WriteLine("Geçersiz Giriş, Tekrar Deneyiniz");
+        }
+
+        return input;
+    }
+
+    public static string GetName()
+    {
+        return Console.ReadLine().Trim();
+    }
+    public static void AddStudent(string name, List<Ogrenci> array)
+    {
+        Ogrenci ogr = new Ogrenci(name);
+        array.Add(ogr);
+
+    }
 }
--- 2-BOLUM/generic-kavrami-012/Program.cs
+++ 2-BOLUM/generic-kavrami-012/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-
+/*
 List<int> degisken = new List<int>();
 degisken.Add(1);
 degisken.Add(10);
@@ -12,3 +12,29 @@ stringKolleksiyon.Add("Fatma");
 List<Ogrenci> ogrenciKolleksiyon = new List<Ogrenci>();
 
 ogrenciKolleksiyon.Add(new Ogrenci() { Id = 10, Name = "Ali" });
+*/
+
+List<Ogrenci> ogrenciler = new();
+
+while (true)
+{
+    int input = Ogrenci.Menu();
+
+    if (input == 1)
+    {
+        var name = Ogrenci.GetName();
+        Ogrenci.AddStudent(name, ogrenciler);
+    }
+    else if (input == 2)
+    {
+
+    }
+    else if (input == 3)
+    {
+
+    }
+    else if (input == 4)
+    {
+
+    }
+}
