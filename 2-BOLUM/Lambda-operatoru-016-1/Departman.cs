c204af9e9908db61875a57f02a6725fbab1e2d97
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2024-11-03 14:10:54 +0300

    asd

--- 2-BOLUM/Lambda-operatoru-016-1/Departman.cs
+++
@@ -1,5 +0,0 @@
-public class Departman
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
--- 2-BOLUM/Lambda-operatoru-016-1/Personel.cs
+++
@@ -1,8 +0,0 @@
-public class Personel
-{
-    public int Id { get; set; }
-    public int departmanId { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public double Salary { get; set; }
-}
