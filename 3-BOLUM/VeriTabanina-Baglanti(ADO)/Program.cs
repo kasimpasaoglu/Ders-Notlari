c08cc71ed0b689d6f79a33c293118256c2073314
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2024-11-13 21:31:16 +0300

    dfg

--- 3-BOLUM/VeriTabanina-Baglanti(ADO)/Program.cs
+++ 3-BOLUM/VeriTabanina-Baglanti(ADO)/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Veri tabanına bağlatı : Ado.net(Eski) kullanarak
+
+
+// sql ile çalışmak için, nugetten bir paket indirmeniz gerekmektedir!!
+// paketin adresi : dotnet add package Microsoft.Data.SqlClient --version 6.0.0-preview2.24304.8 
+// yukarıdaki vermiş olduğum konutu terminalde çalıştırdığınızda artık sql ile çalışabilir hale geleceksiniz!!
+
+using Microsoft.Data.SqlClient; // nugetten sqlClient paketini indiriyoruz
+
+SqlConnection con = new SqlConnection("data source=.;initial catalog=Product;integrated security=true"); //Connection Stringi dogru birsekilde alip buraya ekliyoruz.
+SqlCommand command = new SqlCommand("select product_name from Product", con); // SQL'e bir komut gondermek icin select komutunu bir degiskene tanimladik. Yukaridaki 'con' degiskenini kullanarak select komutunu gonderecek
+
+con.Open(); // yukarda bilgileri girilen db'ye baglanti atiyoruz.
+
+SqlDataReader reader = command.ExecuteReader(); //bu metod surekli, satir satir veri getirecek. 
+while (reader.Read()) // Read() metodu veriyi getirirken, arkada baska veri kaldi mi bilgisini de true ve ya false olarak getirir. Son veri geldiginde false doner. ve dongu biter.
+{
+    Console.WriteLine("Ad : " + reader["product_name"]); // gelen sonucu ekrana yazdirdik. Select Sorgusunda product_name yazdik o yuzden burda da ayni isim olmak zorunda
+}
+reader.Close(); // DB'ye sorguyu birak
+con.Close();  // baglantiyi kes
