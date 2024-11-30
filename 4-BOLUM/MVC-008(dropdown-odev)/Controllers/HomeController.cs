63cb49a5abd1de995a3d77eac5a308126386afd6
Kasim Pasaoglu <kasimpasaoglu@windowslive.com>
2024-11-30 13:26:02 +0300

    ff

--- 4-BOLUM/MVC-008(dropdown-odev)/Controllers/HomeController.cs
+++ 4-BOLUM/MVC-008(dropdown-odev)/Controllers/HomeController.cs
@@ -15,12 +15,46 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        List<City> cities = new List<City>() { Id = "1", Name }
+        List<City> cities = new()
+        {
+            new City(){Id = 1, Name="Istanbul" },
+            new City(){Id = 2, Name="Bursa" }
+        };
+
+        List<Disrict> disricts = new()
+        {
+            new Disrict(){Id=1, CityId=1, Name="Besiktas"},
+            new Disrict(){Id=2, CityId=1, Name="Kadikoy"},
+
+            new Disrict(){Id=3, CityId=2, Name="Nilufer"},
+            new Disrict(){Id=4, CityId=2, Name="Osmangazi"},
+        };
+
+        List<Neighborhood> neighborhoods = new()
+        {
+            new Neighborhood(){Id=1, DisrictId=1, Name="Etiler"},
+            new Neighborhood(){Id=2, DisrictId=1, Name="Gayrettepe"},
+
+            new Neighborhood(){Id=3, DisrictId=2, Name="Acibadem"},
+            new Neighborhood(){Id=4, DisrictId=2, Name="Fenerbahce"},
+
+            new Neighborhood(){Id=5, DisrictId=3, Name="Ozluce"},
+            new Neighborhood(){Id=6, DisrictId=3, Name="Odunluk"},
+
+            new Neighborhood(){Id=7, DisrictId=4, Name="Cekirge"},
+            new Neighborhood(){Id=8, DisrictId=4, Name="Doburca"}
+        };
 
 
+        SelectorViewModel main = new()
+        {
+            Cities = cities,
+            Disricts = disricts,
+            Neighborhoods = neighborhoods
+        };
 
 
-        return View();
+        return View(main);
     }
 
     public IActionResult Privacy()
