76a764cca4bc0c1746726e6733f07f19418f6dbe
Kasım Paşaoğlu <168188063+kasimpasaoglu@users.noreply.github.com>
2024-12-07 12:42:02 +0300

    kkk

---
+++ 4-BOLUM/MVC-014(Action-Filter)/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC_014_Action_Filter_.Models;
+
+namespace MVC_014_Action_Filter_.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ 4-BOLUM/MVC-014(Action-Filter)/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_014_Action_Filter_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
