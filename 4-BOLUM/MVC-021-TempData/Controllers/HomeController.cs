ad41c27599a9070c7c5b4dfbb5906d99410aa69b
Kasim Pasaoglu <kasimpasaoglu@windowslive.com>
2024-12-17 20:02:46 +0300

    df

---
+++ 4-BOLUM/MVC-021-TempData/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC_021_TempData.Models;
+
+namespace MVC_021_TempData.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        TempData["Message"] = "Ben TempData icinde tasinan mesajim";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ 4-BOLUM/MVC-021-TempData/README.md
@@ -0,0 +1,6 @@
+# TempData
+
+- TempData dictionary tipinde bir yapidir.
+- ViewBag ve ViewData'dan ayiran en onemli ozellik redirection ile veri tasiyabilme ozelligidir.
+- Bir action'dan digerine redirect yapildigi zaman ViewData veri tasimaz, ancak TempData ile tasinabilir.
+- Kullanimi basit ve ayni oldugu icin yazmaya gerek duymadim
