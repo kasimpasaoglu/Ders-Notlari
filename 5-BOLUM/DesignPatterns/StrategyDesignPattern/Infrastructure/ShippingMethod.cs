eb33df561bc56606d3fc5ab184cc954a655d13fb
Kasim Pasaoglu <kasimpasaoglu@windowslive.com>
2025-01-08 20:25:27 +0300

    df

--- 5-BOLUM/DesignPatterns/StrategyDesignPattern/Infrastructure/ShippingMethod.cs
+++ 5-BOLUM/DesignPatterns/StrategyDesignPattern/Infrastructure/ShippingMethod.cs
@@ -1,3 +1,9 @@
+// bir sinifin davranisini yapilan secime gore dinamk olarak degistirmek?
+// Content -> Strategy <- ConcreteStrategies
+// content stratejiyi kullanir ve belirlenen stratejiye gore calisir
+// strateji farkli algoritmalari tanimlayan ortak bir interface
+// concratestrategy, somut strateji, strateji interfaceini kullanan fakat icerdigi metodlari her biri kendine gore farkli sonuc veren siniflar
+
 public interface IShippingMethod
 {
     double CalculateShippingCost(double orderAmount);
